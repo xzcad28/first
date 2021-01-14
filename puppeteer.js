add981cdff79abeb5b0f122a107d6b845af71f48
xzcad28 <77439619+xzcad28@users.noreply.github.com>
2021-01-14 18:48:50 +0800

    puppeteer.js

---
+++ puppeteer.js
@@ -0,0 +1,19 @@
+var Tank = mongoose.model('Tank', yourSchema);
+
+var small = new Tank({ size: 'small' });
+small.save(function (err) {
+  if (err) return handleError(err);
+  // saved!
+})
+
+// or
+
+Tank.create({ size: 'small' }, function (err, small) {
+  if (err) return handleError(err);
+  // saved!
+})
+
+Tank.remove({ size: 'large' }, function (err) {
+  if (err) return handleError(err);
+  // removed!
+});
